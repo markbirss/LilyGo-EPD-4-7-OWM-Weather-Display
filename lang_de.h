4d563823837f4bf0b855f81a66f3529c42260df2
Holger Koch <hoko@gmx.de>
2021-03-26 20:38:01 +0100

    Language file for german
    please change #include "lang.h" to #include "lang_de.h" to use it.

---
+++ lang_de.h
@@ -0,0 +1,71 @@
+#define FONT(x) x##_tf
+
+//Temperature - Humidity - Forecast
+const String TXT_FORECAST_VALUES  = "3-Tage Vorschau";
+const String TXT_CONDITIONS       = "Bedingungen";
+const String TXT_DAYS             = "(Tage)";
+const String TXT_TEMPERATURES     = "Temperatur";
+const String TXT_TEMPERATURE_C    = "Temperatur (°C)";
+const String TXT_TEMPERATURE_F    = "Temperatur (°F)";
+const String TXT_HUMIDITY_PERCENT = "Luftfeuchtigkeit (%)";
+
+// Pressure
+const String TXT_PRESSURE         = "Luftdruck";
+const String TXT_PRESSURE_HPA     = "Luftdruck (hPa)";
+const String TXT_PRESSURE_IN      = "Luftdruck (in)";
+const String TXT_PRESSURE_STEADY  = "Stetig";
+const String TXT_PRESSURE_RISING  = "Steigend";
+const String TXT_PRESSURE_FALLING = "Sinkend";
+
+//RainFall / SnowFall
+const String TXT_RAINFALL_MM = "Regen (mm)";
+const String TXT_RAINFALL_IN = "Regen (in)";
+const String TXT_SNOWFALL_MM = "Schnee (mm)";
+const String TXT_SNOWFALL_IN = "Schnee (in)";
+const String TXT_PRECIPITATION_SOON = "Niederschlag";
+
+
+//Sun
+const String TXT_SUNRISE  = "Sonnenauf.";
+const String TXT_SUNSET   = "Sonnenunt.";
+
+//Moon
+const String TXT_MOON_NEW             = "Neumond";
+const String TXT_MOON_WAXING_CRESCENT = "zunehm. Sichel";
+const String TXT_MOON_FIRST_QUARTER   = "erstes Viertel";
+const String TXT_MOON_WAXING_GIBBOUS  = "zunehm. Mond";
+const String TXT_MOON_FULL            = "Vollmond";
+const String TXT_MOON_WANING_GIBBOUS  = "abnehm. Mond";
+const String TXT_MOON_THIRD_QUARTER   = "letztes Viertel";
+const String TXT_MOON_WANING_CRESCENT = "abnehm. Sichel";
+
+//Power / WiFi
+const String TXT_POWER  = "Power";
+const String TXT_WIFI   = "WiFi";
+const char* TXT_UPDATED = "Updated:";
+
+
+//Wind
+const String TXT_WIND_SPEED_DIRECTION = "Wind Geschw./Richtung";
+const String TXT_N   = "N";
+const String TXT_NNE = "NNO";
+const String TXT_NE  = "NO";
+const String TXT_ENE = "ONO";
+const String TXT_E   = "O";
+const String TXT_ESE = "OSO";
+const String TXT_SE  = "SO";
+const String TXT_SSE = "SSO";
+const String TXT_S   = "S";
+const String TXT_SSW = "SSW";
+const String TXT_SW  = "SW";
+const String TXT_WSW = "WSW";
+const String TXT_W   = "W";
+const String TXT_WNW = "WNW";
+const String TXT_NW  = "NW";
+const String TXT_NNW = "NNW";
+
+//Day of the week
+const char* weekday_D[] = { "So", "Mo", "Di", "Mi", "Do", "Fr", "Sa" };
+
+//Month
+const char* month_M[] = { "Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez" };
